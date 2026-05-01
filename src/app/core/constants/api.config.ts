2221e96b9f8dc0119f14ce6175d3487a916633ab
t-h-e-4-5 <theophiledumashie@gmail.com>
2026-05-01 22:43:28 +0000

    Deploy frontend v2.3

--- src/app/core/constants/api.config.ts
+++ src/app/core/constants/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     //BASE_URL: '',
-    BASE_URL: 'http://127.0.0.1:5000/api',
+    BASE_URL: 'http://127.0.0.1:5000/',
     ENDPOINTS: {
         AUTH: {
             LOGIN: '/auth/login',
