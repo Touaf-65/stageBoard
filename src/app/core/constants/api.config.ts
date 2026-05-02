d5b23274145aadbc1beb469b6b9742bf074850cc
t-h-e-4-5 <theophiledumashie@gmail.com>
2026-05-02 10:20:30 +0000

    Deploy frontrnd v2.4.3

--- src/app/core/constants/api.config.ts
+++ src/app/core/constants/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     //BASE_URL: '',
-    BASE_URL: 'http://0.0.0.0:5000/api',
+    BASE_URL: 'http://172.18.0.4:5000/api',
     ENDPOINTS: {
         AUTH: {
             LOGIN: '/auth/login',
